8367a9558a7b65f091680e722ed382dfd2dc765d
Nicat Guliyev <ng@Nicats-MacBook-Pro.local>
2019-05-06 22:38:07 +0400

    Initial Commit

---
+++ Metro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metro
+//
+//  Created by Nicat Guliyev on 5/6/19.
+//  Copyright © 2019 Nicat Guliyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
